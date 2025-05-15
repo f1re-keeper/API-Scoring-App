11102bf9fe512302f2c462ea5f8418743c82c4aa
f1re-keeper <elkvc0@gmail.com>
2025-05-16 02:28:04 +0400

    testing changes 2

--- src/main/java/org/example/theneoassignment/Model/Result.java
+++ src/main/java/org/example/theneoassignment/Model/Result.java
@@ -14,7 +14,6 @@ public class Result {
     @NotNull(message = "Name must not be null")
     private String name;
 
-    //testing changes
     @NotNull(message = "Weight must not be null")
     private int weight;
 
